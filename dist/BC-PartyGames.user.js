7edb9a2aed02b11b1e080882f1bd85a88de17530
Likulisu <awdrrawd1@gmail.com>
2026-08-25 17:50:33 +0800

    updata

--- dist/BC-PartyGames.user.js
+++ dist/BC-PartyGames.user.js
@@ -6,7 +6,7 @@
 // @description  Modular multiplayer party games for Bondage Club. UNO is the first game.
 // @description:zh-TW  在 Bondage Club 聊天室遊玩的模組化多人派對遊戲；首款遊戲為 UNO。
 // @author       Liko
-// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @include      /^https:\/\/(www\.)?(bondage(projects\.elementfx|-(europe|asia))\.com|bondageeurope\.com)\/R*/
 // @grant        none
 // @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
 // @downloadURL  https://raw.githubusercontent.com/awdrrawd/BC-PartyGames/main/dist/BC-PartyGames.user.js
